7a110df7a7ec7679b50c17b2cb678a99e4483e4d
Kam1217 <kamilaostroga@gmail.com>
2025-07-29 15:09:17 +0100

    cleanInput function - splits users input into words based on whitespace

--- main.go
+++ main.go
@@ -10,6 +10,10 @@ func cleanInput(text string) []string{
 	The purpose of this function will be to split the user's input into "words" based on whitespace. 
 	It should also lowercase the input and trim any leading or trailing whitespace.
 	*/
+	lowerStr := strings.TrimSpace(strings.ToLower(text))
+	words := strings.Fields(lowerStr)
+	
+	return words
 }
 
 func main(){
