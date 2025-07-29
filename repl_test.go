64c11af8671c64abd050228d5b814b2130178fed
Kam1217 <kamilaostroga@gmail.com>
2025-07-29 15:23:09 +0100

    Cases for CleanInput test

--- repl_test.go
+++ repl_test.go
@@ -4,6 +4,20 @@ import (
 	"testing"
 )
 
-func TestCleanInput(t *testing.T){
-	
+func TestCleanInput(t *testing.T) {
+	cases := []struct {
+		input    string
+		expected []string
+	}{{
+		input:    "  hello world   ",
+		expected: []string{"hello", "world"},
+	},
+		{
+			input:    "I am making a pokedex   ",
+			expected: []string{"I", "am", "making", "a", "pokedex"},
+		},
+		{
+			input:    "I like Mew and Jigglypuff",
+			expected: []string{"I", "like", "Mew", "and", "Jigglypuff"},
+		}}
 }
