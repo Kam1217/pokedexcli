f0798f91953e540b8a9c93ad7c1ded19712fa23d
Kam1217 <kamilaostroga@gmail.com>
2025-07-29 15:55:53 +0100

    Run tests for CleanInput function - all pass

--- repl_test.go
+++ repl_test.go
@@ -14,10 +14,26 @@ func TestCleanInput(t *testing.T) {
 	},
 		{
 			input:    "I am making a pokedex   ",
-			expected: []string{"I", "am", "making", "a", "pokedex"},
+			expected: []string{"i", "am", "making", "a", "pokedex"},
 		},
 		{
 			input:    "I like Mew and Jigglypuff",
-			expected: []string{"I", "like", "Mew", "and", "Jigglypuff"},
+			expected: []string{"i", "like", "mew", "and", "jigglypuff"},
 		}}
+
+		for _,c := range cases{
+			actual := cleanInput(c.input)
+			if len(actual) != len(c.expected){
+				t.Errorf("length of actual slice: %d, does not match length of expected slice: %d", len(actual), len(c.expected))
+			}
+
+			for i := range actual{
+				word := actual[i]
+				expectedWord := c.expected[i]
+				if word != expectedWord{
+					t.Errorf("Word %s, does not match expected word %s", word, expectedWord)
+				}
+			}
+
+		}
 }
