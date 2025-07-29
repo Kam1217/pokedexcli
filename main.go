a76fd7e984b5dcdb7f95d9401fde566c3ea80166
Kam1217 <kamilaostroga@gmail.com>
2025-07-29 15:02:24 +0100

    Setup cleanInput function

--- main.go
+++ main.go
@@ -1,6 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
+
+func cleanInput(text string) []string{
+	/*
+	The purpose of this function will be to split the user's input into "words" based on whitespace. 
+	It should also lowercase the input and trim any leading or trailing whitespace.
+	*/
+}
 
 func main(){
 	fmt.Println("Hello, World!")
