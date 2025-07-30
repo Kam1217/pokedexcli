6615f78445160eb129ac1021ec4ad77db80c18fb
Kam1217 <kamilaostroga@gmail.com>
2025-07-30 16:26:55 +0100

    LocationAreaResponse struct

---
+++ types.go
@@ -0,0 +1,11 @@
+package main
+
+type LocationAreaResponse struct {
+	Next string `json:"next"`
+	Previous string `json:"previous"`
+	Results [] struct {
+		Name string `json:"name"`
+		Url string `json:"url"`
+	} `json:"results"`  
+}
+
