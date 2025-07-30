80569055ee557ccc99719eba1a0194d40c5daa07
Kam1217 <kamilaostroga@gmail.com>
2025-07-30 11:50:34 +0100

    Help command

--- main.go
+++ main.go
@@ -8,10 +8,12 @@ import (
 )
 
 type cliCommand struct {
-	name string
+	name        string
 	description string
-	callback func() error		
+	callback    func() error
 }
+
+var commands map[string]cliCommand
 
 func cleanInput(text string) []string {
 
@@ -22,55 +24,56 @@ func cleanInput(text string) []string {
 }
 
 func commandExit() error {
-	fmt.Println("Closing the Pokedex...Goodbye!")
+	fmt.Println("Closing the Pokedex... Goodbye!")
 	os.Exit(0)
 	return nil
 }
 
-func commandHelp() error{
-	fmt.Println("Welcome to the Pokedex")
+func commandHelp() error {
 	fmt.Println("Usage:")
 	fmt.Println("")
+	for _, command := range commands {
+		fmt.Printf("%s: %s\n", command.name, command.description)
+	}
 	return nil
 }
 
 func main() {
-	commands := map[string]cliCommand{
+	commands = map[string]cliCommand{
 		"exit": {
-			name: "exit",
+			name:        "exit",
 			description: "Exit the Pokedex",
-			callback: commandExit,
+			callback:    commandExit,
 		},
-		"help":{
-			name: "help",
+		"help": {
+			name:        "help",
 			description: "Displays a help message",
-			callback: commandHelp,
+			callback:    commandHelp,
 		},
 	}
-
 	scanner := bufio.NewScanner(os.Stdin)
-	fmt.Println("Welcome to Pokedex!")
+	fmt.Println("Welcome to the Pokedex!")
 	for scanner.Scan() {
 		input := scanner.Text()
 		args := cleanInput(input)
 
-		if len(input) == 0{
+		if len(input) == 0 {
 			continue
 		}
-		
+
 		cmd, ok := commands[args[0]]
 		if !ok {
 			fmt.Println("Unknown command")
 			continue
 		}
 
-		err := cmd.callback() 
-		if err != nil{
+		err := cmd.callback()
+		if err != nil {
 			fmt.Println("Error: ", err.Error())
 			continue
 		}
 	}
-	
+
 	if err := scanner.Err(); err != nil {
 		fmt.Fprintln(os.Stderr, "error:", err)
 	}
