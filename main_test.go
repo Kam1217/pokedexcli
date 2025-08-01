516a680ecf672c8f9202e3ec2f73ea1d515655b0
Kam1217 <kamilaostroga@gmail.com>
2025-08-01 13:24:48 +0100

    add Test Command Map

--- main_test.go
+++ main_test.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"io"
+	"net/http"
+	"net/http/httptest"
 	"os"
 	"os/exec"
 	"strings"
 	"testing"
+	"time"
+	"github.com/Kam1217/pokedexcli/internal/cache"
 )
 
 func TestCleanInput(t *testing.T) {
@@ -85,4 +89,60 @@ func TestCommandExit(t *testing.T) {
 	}
 
 	t.Fatalf("process run with err %v, want exit status 0", err)
+}
+
+func TestCommandMap(t *testing.T) {
+	mockServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		expectedJSON := `{
+		"next": "https://pokeapi.co/api/v2/location-area?offset=20&limit=20",
+		"previous": null,
+		"results":[
+		{
+			"name": "mock-area-1",
+			"URL":"https://pokeapi.co/api/v2/location-area/1/"
+		},
+		{
+			"name": "mock-area-2",
+			"URL": "https://pokeapi.co/api/v2/location-area/2/"
+		}
+			]
+		}`
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write([]byte(expectedJSON))
+	}))
+
+	defer mockServer.Close()
+
+	oldStdout := os.Stdout
+	r,w,_ := os.Pipe()
+	os.Stdout = w
+
+	cach := cache.NewCache(10 * time.Minute)
+	conf := &Config{
+		Next: mockServer.URL,
+		Previous: "", 
+		Cache : cach,
+	}
+
+	err := commandMap(conf)
+	w.Close()
+	output, _:= io.ReadAll(r)
+	os.Stdout = oldStdout
+	outputStr := strings.TrimSpace(string(output))
+
+	if err != nil{
+		t.Error("Expected no error but go: %w", err)
+	}
+
+	expectedNames := []string{"mock-area-1", "mock-area-2"}
+	for _, name := range expectedNames{
+		if !strings.Contains(outputStr, name){
+			t.Errorf("Expected output to contain %s", name)
+		}
+	}
+
+	if conf.Next != "https://pokeapi.co/api/v2/location-area?offset=20&limit=20"{
+		t.Errorf("Expected Next to be updated")
+	}
 }
