9ded70df04f204ff5fc65cf56c8f8fa57d6035a7
Kam1217 <kamilaostroga@gmail.com>
2025-08-04 16:02:26 +0100

    Update CatchPokemonResponse

--- internal/pokemonclient/types.go
+++ internal/pokemonclient/types.go
@@ -1,28 +1,37 @@
 package pokemonclient
 
 type LocationAreaResponse struct {
-	Next string `json:"next"`
+	Next     string `json:"next"`
 	Previous string `json:"previous"`
-	Results []struct {
+	Results  []struct {
 		Name string `json:"name"`
-		URL string `json:"url"`
-	} `json:"results"`  
+		URL  string `json:"url"`
+	} `json:"results"`
 }
 
 type FindPokemonResponse struct {
 	PokemonEncounters []struct {
 		Pokemon struct {
 			Name string `json:"name"`
-			URL string `json:"url"`
+			URL  string `json:"url"`
 		} `json:"pokemon"`
 	} `json:"pokemon_encounters"`
 }
 
-
-type CatchPokemonResponse struct{
-	BaseExperience int `json:"base_experience"`
-	Forms [] struct{
-		Name string `json:"name"`
-		URL string `json:"url"`
-	} `json:"forms"`
+type CatchPokemonResponse struct {
+	BaseExperience int    `json:"base_experience"`
+	Name           string `json:"name"`
+	Height         int    `json:"height"`
+	Stats          []struct {
+		BaseStat int `json:"base_stat"`
+		Stat     struct {
+			Name string `json:"name"`
+		} `json:"stat"`
+	} `json:"stats"`
+	Types []struct {
+		Type struct {
+			Name string `json:"name"`
+		} `json:"type"`
+	} `json:"types"`
+	Weight int `json:"weight"`
 }
